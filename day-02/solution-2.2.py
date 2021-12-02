365445b51d13713c2500dc37e929105934083192
Taronish Madeka <taronish.madeka@credera.com>
2021-12-02 09:57:11 -0600

    Solution for Day 02.2 - trm

---
+++ day-02/solution-2.2.py
@@ -0,0 +1,21 @@
+input_file = open('input.txt', 'r')
+input_values = [line for line in input_file]
+
+h_position = 0
+depth = 0
+aim = 0
+for i in input_values:
+    x = i.split(" ")
+    direction = x[0]
+    magnitude = int(x[1])
+
+    if direction == 'forward':
+        h_position += magnitude
+        depth += magnitude * aim
+    elif direction == 'down':
+        aim += magnitude
+    elif direction == 'up':
+        aim -= magnitude
+
+# My Solution: 1975421260
+print(h_position * depth)
