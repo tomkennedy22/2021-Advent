f15e372c67d7c55f575fce882b6d569cb5dea53f
Anna Grace Franklin <annagrace.franklin@credera.com>
2021-12-10 18:20:15 -0600

    day 9.1 solution, ag input, and prompt with part 2

--- day-09/solution-9.1.py
+++ day-09/solution-9.1.py
@@ -1,58 +1,69 @@
-# treat each line of input as a list and put the lists into a tuple
-# number the lists 
-# write loop that does: 
-# for each number check the list elements before and after it, and check the lists above and below on the same index
-# if all of those numbers are greater than the number, add it to a list of low points
-
-input_file = open('test_input.txt', 'r')
-#input_values = [list(line.strip().split()) for line in input_file]
+input_file = open('input.txt', 'r')
 input_values= [list(line.strip()) for line in input_file]
 
 for sequence in input_values:
     for i in range(0, len(sequence)):
         sequence[i] = int(sequence[i])
 
-i = 0
-for sequence in input_values:
-    for item in sequence:
-        print(item)
-       
-    #for item in sequence:
-            #print(item)
-            
-def compare_horizontal_neighbors(lst):
-    for i, x in enumerate(lst):
-        if x - lst[i-1] > 0 and x - lst[i+1] < 0:
-            return True
 
-def compare_vertical_neighbors(current_list, list_above, list_below):
-    result = [c < b and c < a for c, b, a in zip(current_list, list_below, list_above)]
+def compare_horizontal_neighbors(current_list, i):
+    if i == 0:
+        if current_list[i] - current_list[i+1] < 0:
+            return True
+        else:
+            return False
+    elif i == len(current_list) - 1:
+        if current_list[i] - current_list[i-1] < 0:
+            return True
+        else:
+            return False
+    elif current_list[i] - current_list[i-1] < 0 and current_list[i] - current_list[i+1] < 0:
+        return True
+    else:
+        return False
+
+def compare_vertical_neighbors(current_list, list_above, list_below, i):
+    results = [c < b and c < a for c, b, a in zip(current_list, list_below, list_above)]
+    result = results[i]
     return result
 
-def find_vertical_neighbors():
-    return True
+def find_current_list(input_values, i):
+    current_list = input_values[i]
+    return current_list
+
+def find_list_above(input_values, i):
+    if i == 0:
+        return [9 for i in range(len(input_values[i]))]
+    else:
+        list_above = input_values[i-1]
+    return list_above 
+
+def find_list_below(input_values, i):
+    if i == len(input_values) - 1:
+        return [9 for i in range(len(input_values[i]))]
+    else:
+        list_below = input_values[i+1]
+    return list_below
 
 
-vertical_result = compare_vertical_neighbors(input_values[2], input_values[1], input_values[3])
-print(vertical_result)
-#>>> a = [0, 4, 10, 100]
+def find_low_points(input_values):
+    low_points = []
+    for i in range(0, len(input_values)):
+        current_list = find_current_list(input_values, i)
+        list_above = find_list_above(input_values, i)
+        list_below = find_list_below(input_values, i)
+        for x in range(0, len(current_list)):
+            vertical_result = compare_vertical_neighbors(current_list, list_above, list_below, x)
+            horizontal_result = compare_horizontal_neighbors(current_list, x)
+            if vertical_result and horizontal_result:
+                low_points.append(current_list[x])
+    return low_points
 
-# basic enumerate without condition:
-#>>> [x - a[i - 1] for i, x in enumerate(a)][1:]
-#[4, 6, 90]
-
-# enumerate with conditional inside the list comprehension:
-#>>> [x - a[i - 1] for i, x in enumerate(a) if i > 0]
-#[4, 6, 90]
-
-# the zip version seems more concise and elegant:
-#>>> [t - s for s, t in zip(a, a[1:])]
-#[4, 6, 90]
+                
+low_points = find_low_points(input_values)
+risk_levels = [x+1 for x in low_points]
+risk_level_sum = sum(risk_levels)
+# Answer: 564
+print(risk_level_sum)
 
 
-
-
-
-
-#lists = input_values
-#print(input_lists)
