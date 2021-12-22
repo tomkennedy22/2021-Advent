7b8adcfa154730a1b4850d9b83100306398d96af
Thomas Kennedy <tkennedy@smu.edu>
2021-12-21 23:15:55 -0600

    Working solution for 16.2

--- day-16/solution-16.2.py
+++ day-16/solution-16.2.py
@@ -1,82 +1,103 @@
-from math import prod
+import datetime
+start_time = datetime.datetime.now()
 
-input_file = open("input.txt")
-hex_data = input_file.readline().strip()
-h_size = len(hex_data) * 4
-binary_string = ( bin(int(hex_data, 16))[2:] ).zfill(h_size)
 
-i = 0
+class Bit_Input:
+  def __init__(self, bit_input):
+    self.bit_input = bit_input
+    self.slice_index = 0
 
-def evaluate_recursive(binary_string):
-    global i
-    print(f'i is {i}')
-    packet_type = binary_string[i+3:i+6]
-    packets = []
-    len_id = binary_string[i+6:i+7]
+  def slice(self, bit_read_count):
+    bit_read_end = self.slice_index + bit_read_count
+    # print('\nSlicing', self.slice_index, bit_read_end, bit_read_count , len(self.bit_input) )
+    # print(f'{" "*self.slice_index }[{" "* (bit_read_count-1)})')
+    # print(self.bit_input)
 
-    #base case
-    if packet_type == '100':
-        literals = []
-        x = i+6
-        x1 = i+11
-        literal_sequence = binary_string[x:x1]
-        while binary_string[x] != '0':
-            literal_sequence = binary_string[x:x1]
-            num_bits = binary_string[x+1:x1]
-            literals.append(num_bits)
-            x = x1
-            x1 += 5
-            next_sequence_starts_at = x
-        if binary_string[x] == '0':
-            last_literal_sequence = binary_string[x:x1]
-            num_bits = binary_string[x+1:x1]
-            literals.append(num_bits)
-            if any(c in binary_string[x1:] for c in '1'):
-                next_starting_position = x1
-                i = next_starting_position
-            else:
-                i = len(binary_string) #?
-                return int("".join(literals), 2)
-        return int("".join(literals), 2)
+    raw_bits = self.bit_input[self.slice_index:bit_read_end]
+    #print(f'raw_bits: [{raw_bits}]')
+    self.slice_index = bit_read_end
 
-    #recursive case
-    
-    if len_id == '0':
-        total_length_of_subpackets = binary_string[i+7:i+21+1]
-        length_as_num = int(total_length_of_subpackets, 2)
-        next_starting_position = i+22
-        end_position = next_starting_position + length_as_num
-        i = next_starting_position
-        while i <= end_position:
-            packets.append(evaluate_recursive(binary_string))
-            print(packets)
 
-    else: 
-        total_number_of_subpackets = binary_string[i+7:i+17+1]
-        num_as_num = int(total_number_of_subpackets, 2)
-        next_starting_position = i+18
-        i = next_starting_position
-        for n in range(num_as_num):
-           packets.append(evaluate_recursive(binary_string))
-           print(packets)
-        
+    return int(raw_bits, 2)
 
-    if packet_type == '000':
-        return sum(packets)
-    elif packet_type == '001':
-        return prod(packets)
-    elif packet_type == '010':
-        return min(packets)
-    elif packet_type == '011':
-        return max(packets)
-    elif packet_type == '101':
-        return int(packets[0] > packets[1])
-    elif packet_type == '110':
-        return int(packets[0] < packets[1])
-    elif packet_type == '111':
-        return int(packets[0] == packets[1])
+#My python versioning is messed up, math.prod doesn't exist in 3.7 and I'm having dependency issues. This is a placeholder for the function
+def prod(arr):
+    product = 1
+    for elem in arr:
+        product *= elem
 
-answer = evaluate_recursive(binary_string)
-print(f'Answer is: {answer}')
+    return product
 
-#answer might be 2975066198
+def evaluate_value(subpackets, packet_type):
+    if packet_type == 0:
+        return sum(subpackets)
+    elif packet_type == 1:
+        return prod(subpackets)
+    elif packet_type == 2:
+        return min(subpackets)
+    elif packet_type == 3:
+        return max(subpackets)
+    elif packet_type == 5:
+        return 1 if subpackets[0]>subpackets[1] else 0
+    elif packet_type == 6:
+        return 1 if subpackets[0]<subpackets[1] else 0
+    elif packet_type == 7:
+        return 1 if subpackets[0]==subpackets[1] else 0
+    else:
+        return 0
+
+
+def find_bits(bit_input):
+  packet_version = bit_input.slice(3)
+  packet_type  = bit_input.slice(3)
+
+  if packet_type == 4:
+    val = 0
+    while True:
+      len_type = bit_input.slice(1)
+      val = val * 16 + bit_input.slice(4)
+      if len_type == 0:
+          return val
+
+  subpackets = []
+  bit_slice_type = bit_input.slice(1)
+  #print('bit_slice', bit_slice)
+  if bit_slice_type == 1:
+    #print('in if')
+    for u in range(0, bit_input.slice(11)):
+      subpackets.append(find_bits(bit_input))
+  else:
+    #print('in else')
+    bit_len = bit_input.slice(15)
+    end_index = bit_input.slice_index + bit_len
+    while bit_input.slice_index < end_index:
+      subpackets.append(find_bits(bit_input))
+
+  return evaluate_value(subpackets, packet_type)
+
+dash_line = f'{"-"*60}'
+print(dash_line)
+
+test_inputs = [
+    ['C200B40A82', 3],
+    ['04005AC33890', 54],
+    ['880086C3E88112', 7],
+    ['CE00C43D881120', 9],
+    ['D8005AC2A8F0', 1],
+    ['F600BC2D8F', 0],
+    ['9C005AC2F8F0', 0],
+    ['9C0141080250320F1802104A08', 1],
+    [open("input.txt").readline().strip(), 'Live result!'],
+]
+for test_input in test_inputs:
+    input_val = test_input[0]
+    input_val = bin(int(input_val, 16))[2:]
+    input_val_leading = input_val.zfill(len(test_input[0])*4)
+
+    BI = Bit_Input(input_val_leading)
+
+    print(f'Bit result & expected: *** {find_bits(BI)}, {test_input[1]} ***')
+
+end_time = datetime.datetime.now()
+print(f'\tExecution time: {int((end_time - start_time).total_seconds() * 1000)}ms'  )
+print(dash_line)
